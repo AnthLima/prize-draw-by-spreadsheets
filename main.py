cfd25f94cbf718cdc9adb3c961e0a273a7d48613
AnthLima <tryout3699@gmail.com>
2023-02-22 23:45:19 -0300

    [feat][main]: Add a account fake sid and auth token , and finally send sms for number

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ account_sid = "ACfcd43b45681dc4434c736a600f57652e"
 # Your Auth Token from twilio.com/console
 auth_token  = "780c136a6b2d713917e5b73340e2db82"
 
+client = Client(account_sid, auth_token)
+
 listOfMonths = ['janeiro','fevereiro','março','abril','maio','junho']
 
 
@@ -14,17 +16,16 @@ for month in listOfMonths:
     if (salesTable['Vendas'] > 55000).any():
         seller = salesTable.loc[salesTable['Vendas'] >= 55000 , 'Vendedor']
         sales = salesTable.loc[salesTable['Vendas'] >= 55000 , 'Vendas']
-        print(f'No mes {month} alguém bateu a meta. Vendedor: {seller}, Vendas: {sales}')
+        message = client.messages.create(
+            to="+5584986460017", 
+            from_="+12762778942",
+            body=f"No mes {month} alguém bateu a meta. Vendedor: {seller}, Vendas: {sales}")
 
 
 
 
 
-client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    to="+5584986460017", 
-    from_="+15017250604",
-    body="Hello from Python!")
+
 
 print(message.sid)
